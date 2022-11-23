f6ad928cc0b8f34c444bb487f0d9b461a7de3f1e
albuquerque-lucas <lucaslpra@gmail.com>
2022-11-23 16:12:00 -0300

    Exercício de validação de triângulo

--- 01-fundamentos/4-introducao-a-javascript/1-primeiros-passos/script.js
+++ 01-fundamentos/4-introducao-a-javascript/1-primeiros-passos/script.js
@@ -1,26 +1,26 @@
-const a = 80;
-const b = 60;
-const c = 20;
+const a = 89;
+const b = 45;
+const c = 45;
 
 function operacao(primeiroNumero, segundoNumero, operador)
 {
 
     switch(operador){
         case '+':
-            console.log(primeiroNumero + segundoNumero);
-            break;
+            return primeiroNumero + segundoNumero;
+            
         case '-':
-            console.log(primeiroNumero - segundoNumero);
-            break;
+            return primeiroNumero - segundoNumero;
+            
         case '*':
-            console.log(primeiroNumero * segundoNumero);
-            break;
+            return primeiroNumero * segundoNumero;
+            
         case '/':
-            console.log(primeiroNumero / segundoNumero);
-            break;
+            return primeiroNumero / segundoNumero;
+            
         case '%':
-            console.log(primeiroNumero % segundoNumero);
-            break;
+            return primeiroNumero % segundoNumero;
+            
     }
 
 }
@@ -40,21 +40,42 @@ function maiorDeTres(primeiroNumero, segundoNumero, terceiroNumero)
 {
     let maior;
 
-    if(maiorNumero(primeiroNumero, segundoNumero) == maiorNumero(primeiroNumero, terceiroNumero))
-    {
+    if(maiorNumero(primeiroNumero, segundoNumero) == maiorNumero(primeiroNumero, terceiroNumero)){
+
         maior = primeiroNumero;
+
     } else if(maiorNumero(segundoNumero, terceiroNumero) == maiorNumero(primeiroNumero, terceiroNumero)){
+        
         maior = terceiroNumero;
+
     } else if(maiorNumero(segundoNumero, terceiroNumero) == maiorNumero(primeiroNumero, segundoNumero)){
+        
         maior = segundoNumero;
+
     }
 
     console.log(maior);
 
 }
 
+function validaTriangulo(angulo1, angulo2, angulo3)
+{
+    let somaDoisLados = operacao(angulo1, angulo2, '+');
 
-maiorDeTres(a, b, c);
+    if(angulo1 <= 0 || angulo2 <= 0 || angulo3 <= 0){
+        console.log("Um dos parâmetros passados possui valor 0. O ângulo precisa ter um valor positivo.")
+    }
+    else if(somaDoisLados + angulo3 !== 180){
+        console.log("A somatória dos ângulos não corresponde a de um triângulo.");
+    }  else{
+        console.log("É um triângulo.");
+    }
+}
+
+validaTriangulo(a, b, c);
+
+
+// maiorDeTres(a, b, c);
 
 // maiorNumero(a, b);
 
