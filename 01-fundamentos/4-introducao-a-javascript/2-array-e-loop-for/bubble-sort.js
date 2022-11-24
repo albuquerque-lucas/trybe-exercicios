5bf79e41fd145ccf9d5e90662e2b23c0b7afa4e0
albuquerque-lucas <lucaslpra@gmail.com>
2022-11-24 11:42:59 -0300

    Exercício bubble-sort ordem decrescente

--- 01-fundamentos/4-introducao-a-javascript/2-array-e-loop-for/bubble-sort.js
+++ 01-fundamentos/4-introducao-a-javascript/2-array-e-loop-for/bubble-sort.js
@@ -1,11 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-
+//ORDEM DECRESCENTE
 for(let i = 1; i < numbers.length; i++){
 
     for(let j = 0; j < i; j++){
 
-        if(numbers[i] < numbers[j]){
+        if(numbers[i] > numbers[j]){
 
             let position = numbers[i];
             numbers[i] = numbers[j];
@@ -18,3 +18,24 @@ for(let i = 1; i < numbers.length; i++){
 }
 
 console.log(numbers);
+
+//ORDEM CRESCENTE
+
+// for(let i = 1; i < numbers.length; i++){
+
+//     for(let j = 0; j < i; j++){
+
+//         if(numbers[i] < numbers[j]){
+
+//             let position = numbers[i];
+//             numbers[i] = numbers[j];
+//             numbers[j] = position;
+
+//         }
+
+//     }
+
+// }
+
+// console.log(numbers);
+
