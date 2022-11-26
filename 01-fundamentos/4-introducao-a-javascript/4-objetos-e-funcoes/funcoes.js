22a2d1a3512009684c14a8abbbe6526084b2b3c6
albuquerque-lucas <lucaslpra@gmail.com>
2022-11-26 18:41:25 -0300

    Exercício 2

--- 01-fundamentos/4-introducao-a-javascript/4-objetos-e-funcoes/funcoes.js
+++ 01-fundamentos/4-introducao-a-javascript/4-objetos-e-funcoes/funcoes.js
@@ -14,3 +14,25 @@ function verifyPalindrome(word)
 
     return verification;
 }
+
+function biggestIndex(array)
+{
+
+    let biggest = array[0];
+
+    for(let i = 1; i < array.length; i += 1){
+
+        if(array[i] > biggest){
+
+            biggestIndex = i;
+        }
+
+    }
+
+    return biggestIndex;
+
+}
+
+const arrayTeste = [2, 3, 6, 7, 10, 1, 55];
+
+console.log(biggestIndex(arrayTeste));
