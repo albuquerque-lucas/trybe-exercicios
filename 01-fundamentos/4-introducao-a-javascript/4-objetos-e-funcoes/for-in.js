108628e27784a6921ef0537f1c4c44f0a885de10
albuquerque-lucas <lucaslpra@gmail.com>
2022-11-26 17:20:26 -0300

    Iniciando exercícios do dia 4. Exercício 1

---
+++ 01-fundamentos/4-introducao-a-javascript/4-objetos-e-funcoes/for-in.js
@@ -0,0 +1,10 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+
+// EXERCÍCIO 1
+
+console.log(`Bem vinda, ${info.personagem}`)
