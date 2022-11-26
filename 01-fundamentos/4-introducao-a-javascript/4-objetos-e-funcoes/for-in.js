064ee508fbd451227b769aba6d6c73e54fec8fc6
albuquerque-lucas <lucaslpra@gmail.com>
2022-11-26 17:42:09 -0300

    Exercícios 1 ao 5

--- 01-fundamentos/4-introducao-a-javascript/4-objetos-e-funcoes/for-in.js
+++ 01-fundamentos/4-introducao-a-javascript/4-objetos-e-funcoes/for-in.js
@@ -2,9 +2,48 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
 };
+
+let infoSecond = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas’',
+    recorrente: 'Sim',
+
+}
 
 
 // EXERCÍCIO 1
 
-console.log(`Bem vinda, ${info.personagem}`)
+//console.log(`Bem vinda, ${info.personagem}`)
+
+
+// EXERCÍCIO 2
+
+//console.log(info['recorrente'])
+
+
+// EXERCÍCIO 3
+
+// for(key in info){
+//     console.log(key)
+// }
+
+
+// EXERCÍCIO 4
+
+// for(key in info){
+//     console.log(info[key])
+// }
+
+
+// EXERCÍCIO 5
+
+// console.log(`${info.personagem} e ${infoSecond.personagem}`);
+// console.log(`${info.origem} e ${infoSecond.origem}`);
+// console.log(`${info.nota} e ${infoSecond.nota}`);
+
+// if(info.recorrente === "Sim" && infoSecond.recorrente === "Sim"){
+//     console.log("Ambos recorrentes");
+// }
