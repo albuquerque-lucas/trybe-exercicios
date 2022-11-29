0e2d5e9f4a676a1d775f130d48d0316722173cc8
albuquerque-lucas <lucaslpra@gmail.com>
2022-11-29 18:42:12 -0300

    Exercício bonus 2

--- 01-fundamentos/4-introducao-a-javascript/3-logica-de-programacao/bonus.js
+++ 01-fundamentos/4-introducao-a-javascript/3-logica-de-programacao/bonus.js
@@ -7,11 +7,10 @@ function retornaQuadrado(numero)
 
     for(let i = 0; i <= numero; i += 1){
         finalString += string;
-    }
-
-    for(let j = 0; j <= numero; j += 1){
         console.log(finalString);
     }
+
+
 
 
 }
