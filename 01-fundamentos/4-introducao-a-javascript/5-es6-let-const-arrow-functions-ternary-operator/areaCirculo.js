a70f4cfdca2702a7d9ae901d0165f85933661502
albuquerque-lucas <lucaslpra@gmail.com>
2022-12-01 16:29:21 -0300

    Exercício areaCirculo concluído

---
+++ 01-fundamentos/4-introducao-a-javascript/5-es6-let-const-arrow-functions-ternary-operator/areaCirculo.js
@@ -0,0 +1,10 @@
+const circleArea = radius => {
+
+    const pi = 3.14;
+
+    return pi * radius * radius;
+
+}
+
+console.log(`A área do círculo é: ${circleArea(3)}`);
+console.log(`A área do círculo é: ${circleArea(5)}`);
