d7287bab0e739b8930b6dd6f17322b2850f429c6
albuquerque-lucas <lucaslpra@gmail.com>
2022-12-06 20:52:37 -0300

    Finalizando exercício do dia

--- 01-fundamentos/5-dom-eventos-web-storage/src/js/app.js
+++ 01-fundamentos/5-dom-eventos-web-storage/src/js/app.js
@@ -6,15 +6,41 @@ const changeText = (tagId, text) => {
 
 }
 
-const changeMainBackground = (mainClass, color) => {
+const changeBackgroundColor = (tagClass, color) => {
 
-    let main = document.querySelector(`.${mainClass}`);
+    let tag = document.querySelector(`.${tagClass}`);
     
-    main.style.backgroundColor = color;
+    tag.style.backgroundColor = color;
+
+}
+
+const changeTagText = (tagName, text) => {
+
+    let tag = document.querySelector(`.${tagName}`);
+    tag.innerText = text;
+
+}
+
+const toUpperCase = id => {
+    let element = document.querySelector(id);
+    element.style.textTransform = 'uppercase';
+}
+
+const logAllItems = tagClass => {
+
+    const item = document.querySelectorAll(tagClass);
+
+    item.forEach(element => {
+        console.log(element.innerText);
+    });
 
 }
 
 let text = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum accusantium quos culpa voluptatibus quo! Numquam similique vel fugit nihil mollitia dolorum adipisci praesentium ea, excepturi iusto voluptatum, hic quidem aperiam?';
 
 changeText('description-foward', text);
-changeMainBackground('main-content', 'rgb(76,164,109)');
+changeBackgroundColor('main-content', 'rgb(76,164,109)');
+changeBackgroundColor('center-content', 'rgb(255,255,255)');
+changeTagText('title', 'Desafio - Javascript');
+toUpperCase('#first-content');
+logAllItems('.content');
