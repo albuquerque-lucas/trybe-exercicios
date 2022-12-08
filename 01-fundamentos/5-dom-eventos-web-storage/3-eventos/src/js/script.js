00a2df06812674c6928d659be732889d535ecc30
albuquerque-lucas <lucaslpra@gmail.com>
2022-12-07 21:08:04 -0300

    Adicionando calendário através da função createDaysOfTheMonth e realizando modificações no estilo da página

--- 01-fundamentos/5-dom-eventos-web-storage/3-eventos/src/js/script.js
+++ 01-fundamentos/5-dom-eventos-web-storage/3-eventos/src/js/script.js
@@ -11,6 +11,40 @@ const createDaysOfTheWeek = () => {
     };
 }
 
+const createDaysOfTheMonth = () => {
+    const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const holidays = [24, 25, 31];
+    const fridays = [3, 11, 18, 25];
+    const ulDays = document.querySelector('#days');
+
+
+    for(let i = 0; i < decemberDaysList.length; i += 1){
+
+        let li = document.createElement('li');
+        li.className = 'days';
+
+        for(let holidayIndex = 0; holidayIndex < holidays.length; holidayIndex += 1){
+            if(decemberDaysList[i] == holidays[holidayIndex]){
+                li.classList.add('holiday');
+            }
+        }
+
+        for(let j = 0; j < fridays.length; j += 1){
+            if(decemberDaysList[i] == fridays[j]){
+                li.classList.add('friday');
+            }
+        }
+
+        li.innerText = decemberDaysList[i];
+        ulDays.appendChild(li);
+
+    }
+
+
+
+}
+
 createDaysOfTheWeek();
+createDaysOfTheMonth();
 
 // Escreva seu código abaixo.
