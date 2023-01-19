7d53c929d64e62f70588ed8e320247b01c57ec7d
albuquerque-lucas <lucaslpra@gmail.com>
2023-01-19 18:41:25 -0300

    Finalizando exercício 3 de Jest

--- 01-fundamentos/6-es6-testes-unitarios/2-jest/2-exercicio-1/functions.test.js
+++ 01-fundamentos/6-es6-testes-unitarios/2-jest/2-exercicio-1/functions.test.js
@@ -1,4 +1,4 @@
-const {myRemove, myFizzBuzz} = require('./functions.js');
+const {myRemove, myFizzBuzz, encode, decode} = require('./functions.js');
 
 describe('Testa a função myRemove()', () => {
   test('Verifica se retorna um array removendo o item correto', () => {
@@ -16,4 +16,31 @@ describe('Testa a função myFizzBuzz', () => {
     expect(myFizzBuzz(20)).toEqual('buzz');
     expect(myFizzBuzz(4)).toEqual(4);
   });
+})
+
+describe('Testa as funções encode e decode', () => {
+  test('Verifica se encode e decode são funções', () => {
+    expect(typeof encode).toEqual('function');
+    expect(typeof decode).toEqual('function');
+  });
+  test('Verifica se as funções possuem os retornos esperados', () => {
+    expect(encode('aeiou')).toEqual("12345");
+    expect(decode('12345')).toEqual("aeiou");
+  });
+  test('Verifica se as demais letras e números não são convertidos', () => {
+    expect(encode('ana')).toEqual('1n1');
+    expect(encode('enu')).toEqual('2n5');
+    expect(encode('amelia')).toEqual('1m2l31');
+    expect(encode('osmar')).toEqual('4sm1r');
+    expect(encode('oswaldo')).toEqual('4sw1ld4');
+    expect(decode('1n1')).toEqual('ana');
+    expect(decode('2nu')).toEqual('enu');
+    expect(decode('1m2l31')).toEqual('amelia');
+    expect(decode('4sm1r')).toEqual('osmar');
+    expect(decode('4sw1ld4')).toEqual('oswaldo');
+  });
+  test('verifica se a string retornada tem o mesmo número de caractéres que a string passada como parâmetro', () => {
+    expect(encode('ana').length).toEqual(3);
+    expect(decode('1n1').length).toEqual(3);
+  })
 })
