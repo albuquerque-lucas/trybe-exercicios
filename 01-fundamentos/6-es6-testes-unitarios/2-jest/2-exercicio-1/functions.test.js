d7f37a6fa6fa071c3429bda215de97cae27b93b9
albuquerque-lucas <lucaslpra@gmail.com>
2023-01-19 17:29:43 -0300

    Refatorando árvore de arquivos para colocar o arquivo package.json na raiz do repositório

--- 01-fundamentos/6-es6-testes-unitarios/2-jest/2-exercicio-1/functions.test.js
+++ 01-fundamentos/6-es6-testes-unitarios/2-jest/2-exercicio-1/functions.test.js
@@ -1,11 +1,9 @@
 const myRemove = require('./functions.js');
 
-describe('a função myRemove()', ()=>{
-  test('verifica se retorna o array esperado', () => {
+describe('a função myRemove()', () => {
+  test('Verifica se retorna um array removendo o item correto', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
-  });
-
-  test('verifica se retorna o array esperado 2', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
-  })
+  });
 })
